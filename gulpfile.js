e85863bed188081d51f69e19e776ea2954506e67
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-16 09:45:27 -0700

    functioning gulpfile! todo: pipe html to public

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,42 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
+const plumber = require('gulp-plumber');
+const del = require('del');
 
-// Pack JS
-function js() {
-    return gulp 
-    .src(['src/scripts/*.js'])
+// Clean Public Directory
+function cleanDirectory() {
+    return del(['public/assets/'])
 }
 
 // Pack CSS
 function css() {
     return gulp
-    .src(['src/css/index.css'])
-    .pipe(gulp.dest('public/index.css'))
+        .src('src/css/index.css')
+        .pipe(gulp.dest('public/assets/'))
+}
+// Pack JS
+function concatScripts() {
+    return (
+        gulp
+            .src(['src/scripts/*.js'])
+            .pipe(plumber())
+            .pipe(concat('index.js'))
+            .pipe(gulp.dest('public/assets/'))
+    );
 }
 
-// Default Task
-gulp.task('default', ['pack-js', 'pack-css']);
+// Watch Files
+function watchFiles() {
+    gulp.watch('src/css/*', css);
+    gulp.watch('src/scripts/*', concatScripts);
+}
+
+// Complex Tasks
+const build = gulp.series(cleanDirectory, gulp.parallel(css, concatScripts));
+
+// Export Tasks
+exports.css = css;
+exports.concatScripts = concatScripts;
+exports.build = build;
+exports.watchFiles = watchFiles;
+exports.default = build;
